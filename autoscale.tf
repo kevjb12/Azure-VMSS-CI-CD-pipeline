8b8fa03a31229fa02bf475c8f4a42de956e0e27f
Kevin Jean-Baptiste <kevin.jb1215@outlook.com>
2026-08-11 16:29:33 -0400

    Update autoscale.tf

--- autoscale.tf
+++ autoscale.tf
@@ -3,7 +3,7 @@ resource "azurerm_monitor_autoscale_setting" "autoscale" {
   name                = "autoscale"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  target_resource_id  = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform_tutorial.id
+  target_resource_id  = azurerm_orchestrated_virtual_machine_scale_set.vmss_terraform.id
   enabled             = true
   profile {
     name = "autoscale"
